9f0a4c1fa2cf8072f2512e0dce597dafe8164a4e
Cody Marshall <codywademarshall@gmail.com>
2026-04-03 02:07:51 -0700

    Integrate Key Vault with managed identity

--- app/index.js
+++ app/index.js
@@ -6,12 +6,25 @@ appInsights.setup()
   .setAutoCollectExceptions(true)
   .start();
 
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('CI/CD is working 🚀');
+const vaultName = "cody-ci-cd-kv-8392";
+const url = `https://${vaultName}.vault.azure.net`;
+const client = new SecretClient(url, new DefaultAzureCredential());
+
+app.get('/', async (req, res) => {
+  try {
+    const secret = await client.getSecret("app-message");
+    res.send(secret.value);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching secret");
+  }
 });
 
 app.listen(port, () => {
